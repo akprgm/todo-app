09f81e4261777ea3e7c6dded4594145adc0f3672
ANIL KUMAR <anilkumar@AutoGrids-MacBook-Pro.local>
2020-05-10 21:51:56 +0530

    :hammer: updated deploy script

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env sh
-echo 'hellow'
+ssh -i ~/.ssh/key.pem ubuntu@52.89.211.197 '
+	cd todo-app
+    sudo git pull origin master
+    sudo npm install
+    sudo npm run build
+    sudo rm -rf /var/www/html/static
+    sudo mv build/* /var/www/html
+'
